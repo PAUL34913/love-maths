c03f8e566ea4df59101bbbd31b350386407a1bc8
Paul Eagle <paul34913@gmail.com>
2022-01-10 23:53:27 +0000

    added code for divide, ensuring num1 > num2 by variable swapping, also applied to subtraction

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,9 +41,17 @@ function runGame(gameType) {
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
     } else if(gameType === 'subtract') {
+        if(num1 < num2) {
+            [num1, num2] = [num2, num1]
+        }
         displaySubtractQuestion(num1, num2);
     } else if(gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
+    } else if(gameType === 'divide') {
+        if(num1 < num2) {
+            [num1, num2] = [num2, num1]
+        }
+        displayDivideQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType} `);
         throw `Unknown game ype: ${gameType}. Aborting!`;
@@ -93,6 +101,10 @@ function calculateCorrectAnswer() {
     else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
     } 
+
+    else if (operator === '/') {
+        return [operand1 / operand2, 'divide'];
+    } 
     
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -136,4 +148,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
 }
